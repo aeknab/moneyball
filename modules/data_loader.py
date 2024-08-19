f1ade13a9ea6eee5e3c88119dbaf954a1347ca63
Andreas Knab <andreas@andreass-mbp.speedport.ip>
2024-08-19 15:33:22 +0200

    file name change

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 from PIL import Image
 
 def load_data():
-    return pd.read_csv('data/BuLi_All_Seasons.csv')
+    return pd.read_csv('data/buli_all_seasons.csv')
 
 def load_colors():
     return pd.read_csv('data/Color_Codes.csv')
